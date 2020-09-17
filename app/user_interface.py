940f3a85414f459da0f95b794bb548d5ced40d6d
gergo225 <fazekasgergo225@gmail.com>
2020-09-17 22:21:32 +0300

    Add images and update UI to display theme names and images

--- app/user_interface.py
+++ app/user_interface.py
@@ -1,5 +1,7 @@
 """ All UI related stuff """
+import os
 import wx
+from app.theme_manager import ThemeManager
 
 
 class MainFrame(wx.Frame):
@@ -8,7 +10,7 @@ class MainFrame(wx.Frame):
     def __init__(self):
         style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX)
         super().__init__(
-            parent=None, title="Spotify Theme Changer", size=(720, 450), style=style
+            parent=None, title="Spotify Theme Changer", size=(1080, 720), style=style
         )
         panel = wx.Panel(self)
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -23,24 +25,22 @@ class MainFrame(wx.Frame):
 
         left_sizer.Add((-1, 16))
 
-        theme_names = [
-            "Bloody",
-            "Sunshine Red",
-            "BreezeLight",
-            "Outrun",
-            "Phosphorus",
-            "Cyberpunk",
-            "Cherry-Blossom",
-        ]
-        self.theme_list = wx.ListBox(panel, choices=theme_names, style=wx.LB_SINGLE)
+        theme_manager = ThemeManager()
+        theme_names = theme_manager.theme_names
+        self.theme_list = wx.ListBox(
+            panel, choices=theme_names, style=wx.LB_SINGLE, size=(185, 250)
+        )
         self.theme_list.SetSelection(0)
         self.theme_list.Bind(wx.EVT_LISTBOX, self.on_list_item_selected)
         left_sizer.Add(self.theme_list)
 
-        self.image_names = [f"image{i}.png" for i in range(7)]
+        self.image_names = []
+        for theme in theme_names:
+            image_path = os.path.join("app", "images", f"{theme}.png")
+            self.image_names.append(image_path)
         image = wx.Image(self.image_names[0], type=wx.BITMAP_TYPE_ANY)
         self.image_control = wx.StaticBitmap(panel, bitmap=wx.Bitmap(image))
-        right_sizer.Add(self.image_control)
+        right_sizer.Add(self.image_control, flag=wx.ALIGN_CENTER)
 
         main_sizer.Add(left_sizer, flag=wx.ALL, border=8)
         main_sizer.Add(right_sizer, flag=wx.EXPAND)
