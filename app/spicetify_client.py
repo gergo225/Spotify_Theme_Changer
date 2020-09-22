09f6f4ee40ac4723b97c32ed1e6f9e3ba4bde003
gergo225 <fazekasgergo225@gmail.com>
2020-09-22 23:15:17 +0300

    Apply a theme using the spicetify client

--- app/spicetify_client.py
+++ app/spicetify_client.py
@@ -2,6 +2,7 @@
 import subprocess
 import os
 import zipfile
+from app.theme_manager import ThemeManager
 
 
 class SpicetifyClient:
@@ -10,16 +11,15 @@ class SpicetifyClient:
     def __init__(self):
         if not self.spicetify_is_installed():
             self.install_spicetify()
-        if not os.listdir(os.path.join(os.getenv("USERPROFILE"), ".spicetify", "Themes")):
-            print("Installing themes")
+        if not os.listdir(
+            os.path.join(os.getenv("USERPROFILE"), ".spicetify", "Themes")
+        ):
             self.install_themes()
-        else:
-            print("Themes already installed")
 
     def spicetify_is_installed(self):
         """Checks if Spicetify is already installed or not and
         returns a boolean value"""
-        output = subprocess.run( # pylint: disable=subprocess-run-check
+        output = subprocess.run(  # pylint: disable=subprocess-run-check
             ["powershell", "spicetify", "--help"], capture_output=True
         )
         return not output.returncode
@@ -45,8 +45,8 @@ class SpicetifyClient:
         # subprocess.run(["spicetify", "enable-devtool"], check=True)
 
     def install_themes(self):
-        """ Extracts the themes.zip containing all themes to the 'Themes'
-        directory in the installed spicetify folder """
+        """Extracts the themes.zip containing all themes to the 'Themes'
+        directory in the installed spicetify folder"""
         themes_zip = os.path.join(os.getcwd(), "app", "Themes.zip")
 
         user_profile = os.getenv("USERPROFILE")
@@ -54,3 +54,22 @@ class SpicetifyClient:
 
         with zipfile.ZipFile(themes_zip, "r") as zip_file:
             zip_file.extractall(spicetify_themes_folder)
+
+    def apply_theme(self, theme_name):
+        """Apply the theme named 'theme_name'. This must be
+        from the theme names list (in ThemeManager)"""
+        theme = ThemeManager.get_theme_code(theme_name)
+        color_scheme = ThemeManager.get_color_scheme_code(theme_name)
+
+        subprocess.run(
+            [
+                "spicetify",
+                "config",
+                "current_theme",
+                theme,
+                "color_scheme",
+                color_scheme,
+            ],
+            check=True,
+        )
+        subprocess.run(["spicetify", "apply"], check=True)
