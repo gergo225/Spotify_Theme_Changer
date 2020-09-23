252b39b3fe1f68c528d3aecd8b06741442a78824
gergo225 <fazekasgergo225@gmail.com>
2020-09-23 11:23:55 +0300

    Initialize spicetify before starting the GUI

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,7 @@
 """ Main file """
 from app.user_interface import UserInterface
+from app.spicetify_client import SpicetifyClient
 
 if __name__ == "__main__":
+    spicetify = SpicetifyClient()
     ui = UserInterface()
